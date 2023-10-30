cc5d0a63bedf5b7da31173df75c6aeea99733270
EsraaHamada100 <esraahamada654@gmail.com>
2023-10-30 15:27:08 +0200

    demo version

---
+++ api/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../my_model_version1")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get('/ping')
+async def ping():
+    return "Hello, I am alive. yeah"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post('/predict')
+async def predict(
+        # file of type UploadFile and its default value is File(...)
+        file: UploadFile = File(...)
+):
+
+    image = read_file_as_image(await file.read())
+    # convert image from something like this [232,543,32] to this [[232,543,32]]
+    # because predict function should be given a list of images not only one image
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(img_batch)  # because he treated it as a batch of image not only one image
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
